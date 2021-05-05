18178da9c59bf01013e74ec51bce74b3f67a64a8
Hari Krishna Raj <hari.krishnaraj@checkpt.com>
2021-05-05 22:17:50 +0530

    Query parameter

--- Server/server.py
+++ Server/server.py
@@ -9,10 +9,16 @@ class htmlRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
 
+class queryParamRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        lang = self.get_arguments('lang')
+        self.write(f'Favorite Language is {lang}')
+
 if __name__ == '__main__' :
     app = tornado.web.Application([
         (r'/', basicRequestHandler),
-        (r'/html', htmlRequestHandler)
+        (r'/html', htmlRequestHandler),
+        (r'/query',queryParamRequestHandler)
     ])
     port = 12345
     app.listen(port)
