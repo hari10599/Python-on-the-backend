3bbb87d3dba597f597400c276209183aa2ef8908
Hari Krishna Raj <hari.krishnaraj@checkpt.com>
2021-05-18 22:55:57 +0530

    Load Balancing using Nginx

---
+++ LoadBalancer/Server.py
@@ -0,0 +1,18 @@
+import tornado.web
+import tornado.ioloop
+import sys
+import os
+
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(f'Hello World : {os.getpid()}')
+
+
+if __name__ == '__main__' :
+    app = tornado.web.Application([
+        (r'/', basicRequestHandler)
+    ])
+    port = sys.argv[1]
+    app.listen(port)
+    print(f'Listening on port {port}')
+    tornado.ioloop.IOLoop.current().start()
